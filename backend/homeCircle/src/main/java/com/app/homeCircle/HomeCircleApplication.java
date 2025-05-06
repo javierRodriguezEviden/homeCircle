f9fa5125d6922ee45e80cacc34992f45b17a12da
Javier.rc <javier.rodriguez.external@eviden.com>
2025-05-06 13:15:11 +0200

    primer commit

---
+++ backend/homeCircle/src/main/java/com/app/homeCircle/HomeCircleApplication.java
@@ -0,0 +1,13 @@
+package com.app.homeCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeCircleApplication.class, args);
+	}
+
+}
---
+++ backend/homeCircle/src/test/java/com/app/homeCircle/HomeCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.homeCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
