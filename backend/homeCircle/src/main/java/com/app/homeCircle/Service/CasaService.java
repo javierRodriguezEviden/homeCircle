ae682e09c4a3f4a1be835c5da29e0e1af3a012ff
Miguel Angel De la Cruz Carmena (ext) <miguel.de.external@eviden.com>
2025-05-07 13:32:39 +0200

    creación CasaService 07.05.25 - 13.32 - MI

---
+++ backend/homeCircle/src/main/java/com/app/homeCircle/Service/CasaService.java
@@ -0,0 +1,30 @@
+package com.app.homeCircle.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.app.homeCircle.Casa.Casa;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CasaService {
+
+    private final CasaRepository casaRepository; /* Añadir el Repositorio */
+
+    public void createCasa(Casa casa) {
+        casaRepository.save(casa);
+    }   
+
+    public void updateCasa(Integer id, Casa casaData) {
+        Casa casa = casaRepository.findById(id).orElseThrow(() -> new RuntimeException("Casa con id: " + id + " no encontrada"));
+
+        casaData.setId(casa.getId());
+        casaRepository.save(casaData);
+    }
+
+    public void deleteCasa(Integer id) {
+        Casa casa = casaRepository.findById(id).orElseThrow(() -> new RuntimeException("Casa con id: " + id + " no encontrada"));
+        casaRepository.delete(casa);
+    }
+}
