d3ab2b4400d693c488792073deb38747e83c88c4
AlbertoEviden <alberto.campanero.external@eviden.com>
2025-05-07 13:53:59 +0200

    Front actualizdo 07.05.25 - 13:53 - AC

--- frontend/homeCircle/src/app/app.module.ts
+++ frontend/homeCircle/src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { HomeComponent } from './home/home.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
+//Import para routes
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+
+
+//Creamos la variable const routes para establecer las rutas
+const appRoutes:Routes=[
+ 
+  //Lo que haremos aqui es crear un objeto para cada ruta
+ 
+  //Como la creamos, con el path como en python y la ubicacion, ya que son componentes -> component
+  {path:'', component:HomeComponent},
+  {path:'sobreNosotros', component:SobreNosotrosComponent},
+  {path:'registro', component:RegistroComponent},
+  {path:'login', component:LoginComponent},
+  //Esto le indica a angular que cualquier cosa diferente a lo visto antes, irá aqui
+ 
+ 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreNosotrosComponent,
+    HomeComponent,
+    RegistroComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    //Para las rutas, para que utilice todas las rutas presentes en la constante
+    RouterModule.forRoot(appRoutes),
+
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
