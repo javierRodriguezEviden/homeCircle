8db711c2a7266982abc2a58306c0d9e957730844
ireneDelEviden <irene.del.external@eviden.com>
2025-05-07 13:37:54 +0200

    Controller guardar cambios - 07/05/25 - 13:35 - IA

---
+++ backend/homeCircle/src/main/java/com/app/homeCircle/Service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.app.homeCircle.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.app.homeCircle.Usuario.Usuario;
+
+import lombok.RequiredArgsConstructor;
+
+@Service 
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository; /* Añadir el Repositorio */
+
+    public void createUsuario(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    public void updateUsuario(Integer id, Usuario usuarioData){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario con id: " + id + " no encontrado"));
+
+        usuarioData.setId(usuario.getId());
+        usuarioRepository.save(usuarioData);
+    }
+
+    public void deleteUsuario(Integer id){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario con id: " + id + " no encontrado"));
+        usuarioRepository.delete(usuario);
+    }
+}
