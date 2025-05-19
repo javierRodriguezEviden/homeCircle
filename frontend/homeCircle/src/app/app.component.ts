182270dfaa121de7fde1fc872bd635429ea0105a
anukiwifresa <anastasia.datsko.external@eviden.com>
2025-05-19 10:56:27 +0200

    Merge con Alberto

--- frontend/homeCircle/src/app/app.component.ts
+++ frontend/homeCircle/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +9,11 @@ import { Router } from '@angular/router';
   standalone: false
 })
 export class AppComponent implements OnInit {
-
+ 
   estaLogueado = false;
-
+ 
   constructor(private authService: AuthService, private router: Router) {}
-
+ 
   ngOnInit(): void {
     // Escuchar cambios en el estado de autenticación
     this.authService.estaLogueado$.subscribe(loggedIn => {
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
       console.log('Estado de autenticación:', this.estaLogueado);
     });
   }
-
+ 
   logout(): void {
     this.authService.logout(); // Borramos el usuario del localStorage y emite false
     this.router.navigate(['/login']); // Redirigir a login una vez que se cierra sesión
